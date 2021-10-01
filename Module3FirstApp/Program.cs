8c0fce96b6e3db024ab96dad4f72e0e1d6dd53d8
ost-rv <ost-rv@mail.ru>
2021-10-01 21:46:16 +0300

    Add project files.

---
+++ Module3FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module3FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Роман";
+
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
