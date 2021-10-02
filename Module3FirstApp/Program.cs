6d11f187421a69d47ba33f0b8fc891cab52419d8
ost-rv <ost-rv@mail.ru>
2021-10-02 08:48:52 +0300

    Modify for task 3.3.2

--- Module3FirstApp/Program.cs
+++ Module3FirstApp/Program.cs
@@ -6,10 +6,17 @@ namespace Module3FirstApp
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Роман";
 
-            Console.WriteLine(myName);
+            string name = "Jane";
+            byte age = 27;
+            bool havePet = true;
+            float size = 37.5f;
+
+            Console.WriteLine("My name is {0}", name);
+            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("Do I have a pet? {0}", havePet);
+            Console.WriteLine("My shoe size is {0}", size);
+
             Console.ReadKey();
         }
     }
